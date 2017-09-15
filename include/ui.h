8d27f7176be7587014ea21ac3b381230e7eec5b2
Sander Mertens <sander.mertens8@gmail.com>
2017-09-14 23:43:55 -0700

    Migrate router to ui package

---
+++ include/ui.h
@@ -0,0 +1,31 @@
+/* ui.h
+ *
+ * This is the main package file. Include this file in other projects.
+ * Only modify inside the header-end and body-end sections.
+ */
+
+#ifndef CORTO_UI_H
+#define CORTO_UI_H
+
+#include <corto/corto.h>
+#include <corto/corto.h>
+#include <corto/ui/_project.h>
+#include <corto/c/c.h>
+#include <corto/httprouter/httprouter.h>
+#include <corto/web/server/server.h>
+
+/* $header() */
+/* Definitions that are required by package headers (native types) go here. */
+/* $end */
+
+#include <corto/ui/_type.h>
+#include <corto/ui/_interface.h>
+#include <corto/ui/_load.h>
+#include <corto/ui/c/_api.h>
+
+/* $body() */
+/* Definitions here that need your package headers go here. */
+/* $end */
+
+#endif
+
---
+++ src/ui.c
@@ -0,0 +1,11 @@
+/* This is a managed file. Do not delete this comment. */
+
+#include <corto/ui/ui.h>
+
+int uiMain(int argc, char *argv[]) {
+
+    /* Insert implementation */
+    
+    return 0;
+}
+
