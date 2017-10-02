c836ab214947cf24a2ea8e4be8aead7ac94e6474
Sander Mertens <sander.mertens8@gmail.com>
2017-10-02 08:19:09 +0000

    Remove test icon from sidebar

--- etc/js/sidebar.js
+++ etc/js/sidebar.js
@@ -8,10 +8,6 @@ Vue.component('sidebar', {
     <div class="sidebar">
       <ul>
         <li><md-icon>search</md-icon></li>
-        <li><md-icon>dashboard</md-icon></li>
       </ul>
-      <svg class="connect-indicator">
-        <circle cx="32" cy="20" r="7" fill="green" @click.native="connect"s>
-      </svg>
     </div>`
 });
